16b303fa15a31232a035bc27f54dd6a1858c6aa0
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-05 14:46:00 +0300

    Add productinfo

---
+++ CrunchRevSite/mainRoutes/marketplace.py
@@ -0,0 +1,64 @@
+"""
+2024, Written by the CrunchRev Authors
+
+Route module description: controls everything under /marketplace/
+"""
+
+from __main__ import *
+
+@app.route("/marketplace/productinfo", methods=settings["HTTPMethods"])
+def prodInfo():
+    assetId = int(request.args.get("assetId"))
+
+    if not assetId:
+        return {"success": False, "message": "400, Please specify assetId argument."}, 400, {"Content-Type": "application/json"}
+
+    gameData = GamesDB.fetchOne(assetId)
+
+    if not gameData:
+        return {"success": False, "message": "404, Game does not exist."}, 404, {"Content-Type": "application/json"}
+
+    assetsData = gameData["assets"]
+
+    name = assetsData[1]
+    description = assetsData[2]
+    creatorId = int(assetsData[4])
+
+    creatorName = UserDB.fetchUser(method=2, userId=creatorId)[0]
+
+    createdAt = assetsData[5]
+    updatedAt = assetsData[6]
+    is_for_sale = assetsData[9]
+
+    if is_for_sale == 1:
+        is_for_sale = True
+    else:
+        is_for_sale = False
+
+    return {
+        "AssetId": assetId,
+        "ProductId": assetId,
+        "Name": str(name),
+        "Description": str(description),
+        "AssetTypeId": 9,
+        "Creator": {
+            "Id": creatorId,
+            "Name": str(creatorName),
+            "CreatorType": "User",
+            "CreatorTargetId": creatorId
+        },
+        "IconImageAssetId": assetId,
+        "Created": str(createdAt),
+        "Updated": str(updatedAt),
+        "PriceInRobux": None,
+        "PriceInTickets": None,
+        "Sales": 0,
+        "IsNew": False,
+        "IsForSale": is_for_sale,
+        "IsPublicDomain": False,
+        "IsLimited": False,
+        "IsLimitedUnique": False,
+        "Remaining": None,
+        "MinimumMembershipLevel": 0,
+        "ContentRatingTypeId": 0
+    }, 200, {"Content-Type": "application/json"}
