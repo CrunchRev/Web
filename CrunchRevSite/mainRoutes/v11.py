e42b9fdefc3350eb6a40de78b490942a59071879
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-05 12:04:39 +0300

    small update to character fetch

--- CrunchRevSite/mainRoutes/v11.py
+++ CrunchRevSite/mainRoutes/v11.py
@@ -10,34 +10,42 @@ from __main__ import *
 @app.route("/v1.1/avatar-fetch/", methods=settings["HTTPMethods"])
 def avatar_fetch():
     userId = request.args.get("userId") or "1"
-    json = {
-        "resolvedAvatarType": "R15",
-        "equippedGearVersionIds": [],
-        "backpackGearVersionIds": [],
-        "accessoryVersionIds": [
-            24112667
-        ],
-        "animations": {
-            "Run": 969731563
-        },
-        "bodyColorsUrl": f"http://www.{settings["URL"]}/asset/BodyColors.ashx?userId={userId}",
-        "bodyColors": {
-            "HeadColor": 194,
-            "LeftArmColor": 194,
-            "LeftLegColor": 102,
-            "RightArmColor": 194,
-            "RightLegColor": 102,
-            "TorsoColor": 23
-        },
-        "scales": {
-            "Height": 1,
-            "Width": 1,
-            "Head": 1,
-            "Depth": 1,
-            "Proportion": 0,
-            "BodyType": 0
+    userInfo = UserDB.fetchUser(method=2, userId=userId)
+
+    json = {}
+
+    if not userInfo:
+        json = {"success": False, "message": "Unknown user or error while fetching."}
+    else:
+        avatarType = userInfo[10]
+        json = {
+            "resolvedAvatarType": str(avatarType),
+            "equippedGearVersionIds": [],
+            "backpackGearVersionIds": [],
+            "accessoryVersionIds": [
+                24112667
+            ],
+            "animations": {
+                "Run": 969731563
+            },
+            "bodyColorsUrl": f"http://www.{settings["URL"]}/asset/BodyColors.ashx?userId={userId}",
+            "bodyColors": {
+                "HeadColor": 194,
+                "LeftArmColor": 194,
+                "LeftLegColor": 102,
+                "RightArmColor": 194,
+                "RightLegColor": 102,
+                "TorsoColor": 23
+            },
+            "scales": {
+                "Height": 1,
+                "Width": 1,
+                "Head": 1,
+                "Depth": 1,
+                "Proportion": 0,
+                "BodyType": 0
+            }
         }
-    }
 
     return json, 200, {'Content-Type': 'application/json'}
 
