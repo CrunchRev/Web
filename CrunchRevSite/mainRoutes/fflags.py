79ef28d4035f2ef17a3de2ad56eed48757e52ba2
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-08 20:46:37 +0300

    add legacy 2018L setting for testing

--- CrunchRevSite/mainRoutes/fflags.py
+++ CrunchRevSite/mainRoutes/fflags.py
@@ -21,6 +21,21 @@ def setting(settingstype):
     
     return file, 200, {'Content-Type': 'application/json'}
 
+@app.route("/Setting/2018LGet/<settingstype>/", methods=settings["HTTPMethods"])
+def setting2018Llegcacy(settingstype):
+    path = f'{settings["FFlags2018LPath"]}{settingstype}.json'
+
+    # print(path)
+
+    if not os.path.exists(path):
+        return {}, 404, {'Content-Type': 'application/json'}
+    
+    with open(path, 'r') as file:
+        file = file.read()
+        file = file.replace("roblox.com", settings["URL"])
+    
+    return file, 200, {'Content-Type': 'application/json'}
+
 @app.route("/v1/settings/application", methods=settings["HTTPMethods"])
 def setting2018L():
     typeOfFFlag = request.args.get("applicationName") or "PCDesktopClient"
