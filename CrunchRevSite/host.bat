8fe37ac11c788bd4d60b185ee846a0b68c68d3d0
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-11 12:03:09 +0300

    change host.bat x2

--- CrunchRevSite/host.bat
+++ CrunchRevSite/host.bat
@@ -9,6 +9,8 @@ if not "%COUNT%"=="0" (
     echo Updates available. Stashing local changes and pulling latest updates...
     git stash
     git pull
+) else (
+    echo No updates found.
 )
 
 python main.py
