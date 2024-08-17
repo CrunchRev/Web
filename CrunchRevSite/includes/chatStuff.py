a0d4546247b550a9a8b65df7534be85711634278
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-17 19:12:05 +0300

    add more swear words

--- CrunchRevSite/includes/chatStuff.py
+++ CrunchRevSite/includes/chatStuff.py
@@ -8,13 +8,16 @@ from better_profanity import profanity
 
 class TextFilter:
     def __init__(self):
-        profanity.load_censor_words()
+        profanity.load_censor_words(whitelist_words=["fuck", "f3ck"])
         profanity.add_censor_words([
             "пиздец", "п1зд3ц", "п1здец", "пизд3ц", 
             "ебать", "3бать", "eбать", "еб@ть",
             "сука", "с_ка", "су_ка", "сук@",
             "блядь", "6лядь", "бля_дь", "бл@дь",
             "хуй", "хyй", "х_й", "ху1"
+            "neonazi", "ne0nazi", "neonaz1", "n3onaz1",
+            "n30naz1", "dogshit", "dogsh1t", "d0gshit",
+            "d0gsh1t"
         ])
 
         return None
