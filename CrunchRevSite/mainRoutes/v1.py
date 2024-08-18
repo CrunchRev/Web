b0ea0624d1dab2a29119568224d733baa9108070
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-18 16:38:08 +0300

    add 2018L dev console endpoint

--- CrunchRevSite/mainRoutes/v1.py
+++ CrunchRevSite/mainRoutes/v1.py
@@ -10,3 +10,9 @@ from __main__ import *
 def getAll():
     games = GamesDB.fetchAll()
     return jsonify(games)
+
+# VisualPlugin sent me this endpoint for 2018 developer console
+@app.route("/v1/user/<playerId>/is-admin-developer-console-enabled", methods=settings["HTTPMethods"])
+@app.route("/v1/user/<playerId>/is-admin-developer-console-enabled/", methods=settings["HTTPMethods"])
+def isDevConsoleEnabled2018L(playerId):
+    return jsonify({"isAdminDeveloperConsoleEnabled": True}), 200
