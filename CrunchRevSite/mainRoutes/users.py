419c8df6132466c3dca3ca56e8a2a45a919d0a86
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-18 16:53:12 +0300

    fix

--- CrunchRevSite/mainRoutes/users.py
+++ CrunchRevSite/mainRoutes/users.py
@@ -10,7 +10,7 @@ from __main__ import *
 @app.route("/users/<playerId>/canmanage/<placeId>/", methods=settings["HTTPMethods"])
 def ownership(playerId, placeId):
     user_info = UserDB.fetchUser(method=2, userId=playerId)
-    place_info = GamesDB.fetchOne(placeId)
+    game_data = GamesDB.fetchOne(placeId)
 
     if game_data["assets"] is None or game_data["info"] is None or user_info is None:
         return jsonify({"error": "404, The game or user does not exist."}), 404
