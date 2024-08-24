779566494eab75b3ba787f8d811e846614042151
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-24 18:15:34 +0300

    fixes

--- CrunchRevSite/includes/safeTokensStuff.py
+++ CrunchRevSite/includes/safeTokensStuff.py
@@ -8,29 +8,28 @@ import threading
 import time
 import secrets
 
-avaliable_tokens = []
-
 class Token:
     def __init__(self):
+        self.avaliable_tokens = []
         return None
     
     def internal_add_temporary_item(self, item, timeout=60):
-        avaliable_tokens.append(item)
+        self.avaliable_tokens.append(item)
         threading.Timer(timeout, self.internal_remove_item, [item]).start()
 
     def internal_remove_item(self, item):
-        if item in temp_list:
-            temp_list.remove(item)
+        if item in self.avaliable_tokens:
+            self.avaliable_tokens.remove(item)
 
     def generateToken(self):
         secretToken = secrets.token_hex(64)
 
-        self.internal_add_temporary_item(secretToken, timeout=180)
+        self.internal_add_temporary_item(secretToken, timeout=120)
         return secretToken
 
     def checkToken(self, token):
         if token in avaliable_tokens:
-            avaliable_tokens.remove(token)
+            self.avaliable_tokens.remove(token)
             return True
         else:
             return False
