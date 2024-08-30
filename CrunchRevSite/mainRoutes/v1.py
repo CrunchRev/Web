96f54746702eefb8e13708a0dbaf72fc585e6f43
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-30 20:47:10 +0300

    "Added new route /v1/avatar-fetch to fetch user avatar information"

--- CrunchRevSite/mainRoutes/v1.py
+++ CrunchRevSite/mainRoutes/v1.py
@@ -20,3 +20,51 @@ def isDevConsoleEnabled2018L(playerId):
     is_an_admin = True if user_info[8] == 1 else False
 
     return jsonify({"isAdminDeveloperConsoleEnabled": user_info}), 200
+
+@app.route("/v1/avatar-fetch", methods=settings["HTTPMethods"])
+@app.route("/v1/avatar-fetch/", methods=settings["HTTPMethods"])
+def avatar_fetch():
+    userId = request.args.get("userId", 0)
+    userInfo = UserDB.fetchUser(method=2, userId=userId)
+
+    json = {}
+    charappItemzzz = []
+
+    assetsCharAppFetch = Assets.fetchCharacterApperanceList(userId)
+
+    if assetsCharAppFetch:
+        for assetIdTuple in assetsCharAppFetch:
+            assetId = assetIdTuple[0]
+            charappItemzzz.append(assetId)
+
+    if not userInfo:
+        json = {"success": False, "message": "Unknown user or error while fetching."}
+    else:
+        avatarType = userInfo[10]
+        json = {
+            "resolvedAvatarType": str(avatarType),
+            "equippedGearVersionIds": [],
+            "backpackGearVersionIds": [],
+            "accessoryVersionIds": charappItemzzz,
+            "animations": {
+                "Run": 969731563
+            },
+            "bodyColors": {
+                "HeadColor": 194,
+                "LeftArmColor": 194,
+                "LeftLegColor": 102,
+                "RightArmColor": 194,
+                "RightLegColor": 102,
+                "TorsoColor": 23
+            },
+            "scales": {
+                "Height": 1,
+                "Width": 1,
+                "Head": 1,
+                "Depth": 1,
+                "Proportion": 0,
+                "BodyType": 0
+            }
+        }
+
+    return json, 200, {'Content-Type': 'application/json'}
