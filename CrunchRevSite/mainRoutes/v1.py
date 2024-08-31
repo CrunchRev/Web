4c2c964eec889926b13f8c7f41cc371d2821e96f
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-08-31 21:26:04 +0300

    "Updated isDevConsoleEnabled endpoint: renamed function, removed year from comment, and changed return value to use is_an_admin variable"

--- CrunchRevSite/mainRoutes/v1.py
+++ CrunchRevSite/mainRoutes/v1.py
@@ -11,15 +11,15 @@ def getAll():
     games = GamesDB.fetchAll()
     return jsonify(games)
 
-# VisualPlugin sent me this endpoint for 2018 developer console
+# VisualPlugin sent me this endpoint for developer console
 @app.route("/v1/user/<playerId>/is-admin-developer-console-enabled", methods=settings["HTTPMethods"])
 @app.route("/v1/user/<playerId>/is-admin-developer-console-enabled/", methods=settings["HTTPMethods"])
-def isDevConsoleEnabled2018L(playerId):
+def isDevConsoleEnabled(playerId):
     user_info = UserDB.fetchUser(method=2, userId=playerId)
 
     is_an_admin = True if user_info[8] == 1 else False
 
-    return jsonify({"isAdminDeveloperConsoleEnabled": user_info}), 200
+    return jsonify({"isAdminDeveloperConsoleEnabled": is_an_admin}), 200
 
 @app.route("/v1/avatar-fetch", methods=settings["HTTPMethods"])
 @app.route("/v1/avatar-fetch/", methods=settings["HTTPMethods"])
