eb81881f8d0a82934f01b8786488556e475346d9
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-09-21 21:49:41 +0300

    "Added 'tos' to the list of exempted paths for banned users"

--- CrunchRevSite/mainRoutes/rootpath.py
+++ CrunchRevSite/mainRoutes/rootpath.py
@@ -27,7 +27,7 @@ def setup():
             is_banned = info[5] == 1
 
             if is_banned:
-                if (not request.path.lstrip('/') == "not-approved") and (not "staticContent" in request.path.lstrip('/')):
+                if (not request.path.lstrip('/') == "not-approved") and (not "staticContent" in request.path.lstrip('/')) and (not request.path.lstrip('/') == "tos"):
                     return redirect("/not-approved")
     
 @app.errorhandler(404)
