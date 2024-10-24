58257813ce2159802ebf0d01bd5bd454b26b8eb5
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-10-24 14:32:42 +0300

    fixes

--- CrunchRevSite/mainRoutes/ide.py
+++ CrunchRevSite/mainRoutes/ide.py
@@ -4,6 +4,8 @@
 Route module description: controls everything under /ide/
 """
 
+from __main__ import *
+
 @app.route("/ide/welcome", methods=["GET"])
 def welcomeide():
     return render_template("welcomeide.html")
