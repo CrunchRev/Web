418726cc8fd3c56f096dd2c76c8442b6d61db7a1
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-11-03 18:54:15 +0300

    :D

--- CrunchRevSite/mainRoutes/rootpath.py
+++ CrunchRevSite/mainRoutes/rootpath.py
@@ -272,12 +272,7 @@ def assetdelivery():
     local_path = os.path.join(app.root_path, "LocalAssets")
     local_file_path = os.path.join(local_path, str(idarg))
         
-    ip_address = request.remote_addr
-
-    if 'X-Real-IP' in request.headers:
-        ip_address = request.headers["X-Real-IP"]
-    elif 'X-Real-Ip' in request.headers:
-        ip_address = request.headers["X-Real-Ip"]
+    ip_address = request.headers.getlist("X-Real-IP")[0]
 
     if os.path.isfile(local_file_path):
         return send_from_directory(local_path, str(idarg))
