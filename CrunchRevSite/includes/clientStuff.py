a0581bd77c73c8b192987cb08bd7b28e399b5d6e
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-11-05 20:27:06 +0300

    quick fix asap

--- CrunchRevSite/includes/clientStuff.py
+++ CrunchRevSite/includes/clientStuff.py
@@ -152,7 +152,7 @@ class Arbiter:
             sm_logger.info("Requesting...")
 
             sql2 = "SELECT jobId FROM `jobs_in_use` WHERE `place_id` = %s AND `status` = 0 OR `status` = 1" # idfk how to do that
-            execution2 = self.db.execute_securely(sql2, (int(placeID)))
+            execution2 = self.db.execute_securely(sql2, (placeID,))
 
             if execution2 is not None:
                 try:
