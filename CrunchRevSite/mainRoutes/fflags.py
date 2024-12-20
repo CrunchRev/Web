e1f1e7d462375ddca0b49885663e8406ddf5e46d
yakovexplorer <yakov.zhuravlevich@mail.ru>
2024-12-20 19:40:08 +0300

    update fflags

--- CrunchRevSite/mainRoutes/fflags.py
+++ CrunchRevSite/mainRoutes/fflags.py
@@ -1,7 +1,7 @@
 """
 2024, Written by the CrunchRev Authors
 
-Route module description: controls client settings (feature flags)
+Route module description: controls client settings (feature or fast flags)
 """
 
 from __main__ import *
@@ -10,31 +10,26 @@ from __main__ import *
 def setting(settingstype):
     path = f'{settings["FFlagsPath"]}{settingstype}.json'
 
-    # print(path)
-
     if not os.path.exists(path):
-        return {}, 404, {'Content-Type': 'application/json'}
+        return jsonify({}), 404
     
     with open(path, 'r') as file:
         file = file.read()
         file = file.replace("roblox.com", settings["URL"])
     
-    return file, 200, {'Content-Type': 'application/json'}
+    return jsonify(file), 200
 
 @app.route("/Setting/2018LGet/<settingstype>/", methods=settings["HTTPMethods"])
 def setting2018Llegcacy(settingstype):
     path = f'{settings["FFlags2018LPath"]}{settingstype}.json'
-
-    # print(path)
-
     if not os.path.exists(path):
-        return {}, 404, {'Content-Type': 'application/json'}
+        return jsonify({}), 404
     
     with open(path, 'r') as file:
         file = file.read()
         file = file.replace("roblox.com", settings["URL"])
     
-    return file, 200, {'Content-Type': 'application/json'}
+    return jsonify(file), 200
 
 @app.route("/v1/settings/application", methods=settings["HTTPMethods"])
 @app.route("/v1/settings/application/", methods=settings["HTTPMethods"])
@@ -42,13 +37,11 @@ def setting2021E():
     typeOfFFlag = request.args.get("applicationName") or "PCDesktopClient"
     path = f'{settings["FFlags2021EPath"]}{typeOfFFlag}.json'
 
-    # print(path)
-
     if not os.path.exists(path):
-        return {}, 404, {'Content-Type': 'application/json'}
+        return jsonify({}), 404
     
     with open(path, 'r') as file:
         file = file.read()
         file = file.replace("roblox.com", settings["URL"])
     
-    return file, 200, {'Content-Type': 'application/json'}
+    return jsonify(file), 200
